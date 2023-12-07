0ee1404498c78c7ffe1568ec035f6061668b5367
AnaByo <anaalice1224@outlook.com>
2023-12-07 16:27:32 -0300

    app coffee

---
+++ constants/index.js
@@ -0,0 +1,26 @@
+export const categories = [
+    {
+        id: 1,
+        title: "Cappucino",
+    },
+    {
+        id: 2,
+        title: "Latte",
+    },
+    {
+        id: 3,
+        title: "Expresso",
+    },
+    {
+        id: 4,
+        title: "Mocha",
+    },
+    {
+        id: 5,
+        title: "Americano",
+    },
+]
+
+export const coffeItems = [
+    
+]
